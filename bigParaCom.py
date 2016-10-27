7dee0d69ab6834b2a6eb5268473728e7e9c43702
Ersin MERTOĞLU <aydinligin.umudu@gmail.com>
2016-10-27 03:46:07 +0300

    Update bigParaCom.py

--- bigParaCom.py
+++ bigParaCom.py
@@ -6,9 +6,10 @@ liste=["Kuyumcu Alis","Kuyumcu Satis"]
 # gerekli olan adres
 website=urllib.urlopen("http://www.bigpara.com/altin/ceyrek-altin-fiyati")
 
+#buradan tüm datayı okuduk
 htmltext=website.read()
 
-# site icinde altin fiyatinin bulundugu alan
+# site icinde altin fiyatinin bulundugu alani sectik
 getinspect='<span class="value up">(.+?)</span>'
 
 pattern=re.compile(getinspect)
